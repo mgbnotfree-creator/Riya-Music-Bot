23c308ea219fa8ceb04301230ffa7b37c97f9405
mgbnotfree-creator <mgbnotfree@gmail.com>
2026-09-25 00:05:39 +0530

    update config.py

--- config.py
+++ config.py
@@ -57,7 +57,7 @@ class Config:
 
         # ArtistBots API @ArtistApibot
         self.ARTISTBOTS_API_URL: str = getenv("ARTISTBOTS_API_URL", "https://api01.shrutibots.site")
-        self.ARTISTBOTS_KEY: str = getenv("ARTISTBOTS_KEY", "ShrutiBotsxuXu3CMZmfP6UVA1kQGW")
+        self.ARTISTBOTS_KEY: str = getenv("ARTISTBOTS_KEY", "ShrutiBotsaozDQnr5GA0Qh2dKoDww")
         self.ENABLE_API: bool = self._str_to_bool(getenv("ENABLE_API", "True"))
         self.ENABLE_COOKIES_FALLBACK: bool = self._str_to_bool(getenv("ENABLE_COOKIES_FALLBACK", "True"))
         self.API_TIMEOUT: int = int(getenv("API_TIMEOUT", "60"))
