6a5315ad16019cbf990cc2ac7179e0b120683274
mgbnotfree-creator <mgbnotfree@gmail.com>
2026-09-24 23:06:09 +0530

    update _inline.py

--- Elevenyts/helpers/_inline.py
+++ Elevenyts/helpers/_inline.py
@@ -218,6 +218,7 @@ class Inline:
             ],
             [
                 self.ikb(text=lang["help"], callback_data="help", style=ButtonStyle.SUCCESS),
+                self.ikb(text="ꜱᴏᴜʀᴄᴇ", url="https://github.com/mgbnotfree-creator/Riya-Music-Bot", style=ButtonStyle.DANGER),
                 self.ikb(text="ʟᴀɴɢꜱ", callback_data="help_langs", style=ButtonStyle.PRIMARY),
             ],
             [
@@ -236,3 +237,4 @@ class Inline:
                 ],
             ]
         )
+        
